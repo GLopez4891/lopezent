958a6266919a67c0209f86199b6e72ac7442a99b
Glenn lopez <glenn.lopez2015@yahoo.com>
2018-03-11 21:41:23 -0400

    A simple update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('This is the Lopez ENT App');
+    res.send('It works!');
 });
 
 app.listen(3000, () => console.log('Server running un port 3000'));
